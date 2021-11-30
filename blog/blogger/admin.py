c06b05391ddbd83d639fe58bed2afb54270cef9a
Abishek PS <68456941+a2bshek@users.noreply.github.com>
2021-11-30 11:06:35 +0530

    Delete admin.py

--- blog/blogger/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from blogger import models
-
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title','timestamp','username')
-    def __str__(self):
-        return self.title
-        
-admin.site.register(models.BlogPost,BlogPostAdmin)
